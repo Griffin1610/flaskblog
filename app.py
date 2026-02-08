48a1c3e6ffd50b89bd1c54475569fadba357b944
Griffin1610 <PollyGS14@uww.edu>
2026-02-08 16:30:07 -0600

    wt-forms + input validation

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from flask_bootstrap import Bootstrap5
+from forms import RegistartionForm, LoginForm
+from dotenv import load_dotenv
+import os
+load_dotenv()
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
+
+app.config['SECRET_KEY']= os.getenv('SECRET_KEY')
 
 posts = [
     {
@@ -27,6 +33,24 @@ def home():
 def about():
     return render_template("about.html", title='About')
 
+@app.route("/register", methods=["GET","POST"])
+def register():
+    form = RegistartionForm()
+    if form.validate_on_submit():
+        flash(f"account created for {form.username.data}", 'success')
+        return redirect(url_for('home'))
+    return render_template("register.html", title="Register", form=form)
+
+@app.route("/login", methods=["GET","POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@blog.com" and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template("login.html", title="Login", form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
